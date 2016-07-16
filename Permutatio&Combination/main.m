45811db9f0626a4293a3d66b2cd1a01ea9d3b6d7
Victor <victor@Victors-MacBook-Pro.local>
2016-07-17 00:12:27 +0100

    Initial Commit

---
+++ Permutatio&Combination/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Permutatio&Combination
+//
+//  Created by Victor on 7/17/16.
+//  Copyright (c) 2016 Victor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
